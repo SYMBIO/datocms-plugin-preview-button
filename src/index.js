51fadfb8fd1ad63fb000a0e13e4cd4501ede66bb
Martin Cajthaml <martin.cajthaml@symbio.agency>
2019-11-01 15:41:15 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import './style.css';
+
+window.DatoCmsPlugin.init((plugin) => {
+  plugin.startAutoResizer();
+
+  const container = document.createElement('div');
+  container.classList.add('container');
+
+  const { baseUrl } = plugin.parameters.global;
+  const button = document.createElement('button');
+  button.type = 'button';
+  button.onclick = (e) => {
+    const slug = plugin.getFieldValue('slug.cs');
+    const id = plugin.itemId;
+    switch (plugin.itemType.id) {
+      case '111456':
+        window.open(`${baseUrl}/cs/predstaveni/${slug}-${id}`);
+        break;
+
+      case '107442':
+        window.open(`${baseUrl}/cs/umelec/${slug}-${id}`);
+        break;
+
+      default:
+        break;
+    }
+    e.preventDefault();
+    return false;
+  };
+  button.textContent = 'Zobrazit náhled';
+  container.appendChild(button);
+
+  document.body.appendChild(container);
+});
