b095d5a3ec5312b5e556dfdeefe7bc70da99eb7e
Martin Cajthaml <martin.cajthaml@symbio.agency>
2020-07-07 10:12:46 +0200

    v0.1.0

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ window.DatoCmsPlugin.init((plugin) => {
   const container = document.createElement('div');
   container.classList.add('container');
 
-  const { baseUrl } = plugin.parameters.global;
+  const { baseUrl, label } = plugin.parameters.global;
   const button = document.createElement('button');
   button.type = 'button';
   button.className = 'DatoCMS-button DatoCMS-button--primary';
@@ -14,58 +14,11 @@ window.DatoCmsPlugin.init((plugin) => {
     const { locale } = plugin;
     const slug = plugin.getFieldValue(`slug.${locale}`);
     const url = plugin.getFieldValue(`url.${locale}`);
-    const id = plugin.itemId;
-    switch (plugin.itemType.id) {
-      case '111456': {
-        const path = locale === 'en' ? 'show' : 'predstaveni';
-        window.open(`${baseUrl}/${locale}/${path}/${slug}-${id}`);
-        break;
-      }
-
-      case '111455': {
-        const path = locale === 'en' ? 'show' : 'predstaveni';
-        const production = plugin.getFieldValue('production');
-        const startAt = plugin
-          .getFieldValue('start_at')
-          .replace(':', '-')
-          .replace('T', '-')
-          .substr(0, 16);
-        window.open(
-          `${baseUrl}/${locale}/${path}/nahled-${production}?t=${startAt}`,
-        );
-        break;
-      }
-
-      case '107442': {
-        const path = locale === 'en' ? 'profile' : 'profil';
-        window.open(`${baseUrl}/${locale}/${path}/${slug}-${id}`);
-        break;
-      }
-
-      case '99631':
-        window.open(`${baseUrl}/${locale}/${url}`);
-        break;
-
-      case '114410': {
-        const path = locale === 'en' ? 'news' : 'aktuality';
-        window.open(`${baseUrl}/${locale}/${path}/${slug}`);
-        break;
-      }
-
-      case '111481': {
-        const path = locale === 'en' ? 'press-releases' : 'tiskove-zpravy';
-        window.open(`${baseUrl}/${locale}/${path}/${slug}`);
-        break;
-      }
-
-      default:
-        window.open(`${baseUrl}?locale=${locale}&type=${plugin.itemType.id}&id=${plugin.itemId}&slug=${slug}&url=${url}`);
-        break;
-    }
+    window.open(`${baseUrl}?locale=${locale}&type=${plugin.itemType.id}&id=${plugin.itemId}&slug=${slug}&url=${url}`);
     e.preventDefault();
     return false;
   };
-  button.textContent = 'Zobrazit náhled';
+  button.textContent = label || 'Zobrazit náhled';
   container.appendChild(button);
 
   document.body.appendChild(container);
