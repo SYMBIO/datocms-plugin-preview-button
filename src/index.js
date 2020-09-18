498cddd7bd1d04fcdcb0f49e5648cb9becad210d
Martin Cajthaml <martin.cajthaml@symbio.agency>
2020-09-18 07:48:24 +0200

    prettier

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import "./style.css";
+import './style.css';
 
 window.DatoCmsPlugin.init((plugin) => {
   plugin.startAutoResizer();
 
-  const container = document.createElement("div");
-  container.classList.add("container");
+  const container = document.createElement('div');
+  container.classList.add('container');
 
   const { baseUrl, label } = plugin.parameters.global;
-  const button = document.createElement("button");
-  button.type = "button";
-  button.className = "DatoCMS-button DatoCMS-button--primary";
+  const button = document.createElement('button');
+  button.type = 'button';
+  button.className = 'DatoCMS-button DatoCMS-button--primary';
   button.onclick = (e) => {
     const locale = plugin.locale || plugin.site.attributes.locales[0];
     const slug = plugin.getFieldValue(`slug.${locale}`);
     const url = plugin.getFieldValue(`url.${locale}`);
     window.open(
-      `${baseUrl}?locale=${locale}&type=${plugin.itemType.id}&id=${plugin.itemId}&slug=${slug}&url=${url}`
+      `${baseUrl}?locale=${locale}&type=${plugin.itemType.id}&id=${plugin.itemId}&slug=${slug}&url=${url}`,
     );
     e.preventDefault();
     return false;
   };
-  button.textContent = label || "Zobrazit náhled";
+  button.textContent = label || 'Zobrazit náhled';
   container.appendChild(button);
 
   document.body.appendChild(container);
